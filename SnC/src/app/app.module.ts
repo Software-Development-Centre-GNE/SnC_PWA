826f7b02afc0caf31c064384798f95ee0d283dc5
Jobanpreet <jobanjatt113@gmail.com>
2023-02-23 06:20:15 +0530

    firebase installation

--- SnC/src/app/app.module.ts
+++ SnC/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { environment } from '../environments/environment';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,8 +16,17 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
 	entryComponents: [],
-	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-	bootstrap: [AppComponent]
+	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AngularFirestore,
+		],
+	bootstrap: [AppComponent],
+	imports: [
+		BrowserModule,
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFireAuthModule,
+		AngularFireMessagingModule,
+		AngularFireStorageModule,
+		IonicModule.forRoot(), AppRoutingModule, HttpClientModule
+	  ]
 })
+
 export class AppModule {}
